2da3c4e646bac94adf0a46aceb8edd308e258362
unknown <mbellman@nwe.com>
2021-03-09 23:28:24 -0600

    Playing around with more GSC mapgen

--- src/worldmaps/GscWorldMap.ts
+++ src/worldmaps/GscWorldMap.ts
@@ -1,6 +1,6 @@
-import AbstractWorldMap, { Modification } from './AbstractWorldMap';
+import AbstractWorldMap from './AbstractWorldMap';
 import TileSet from '../TileSet';
-import { Area, Point } from '../types';
+import { Area, Point, Size } from '../types';
 
 export enum GscTile {
   GRASS = 0,
@@ -14,8 +14,30 @@ export enum GscTile {
   LEDGE_BOTTOM_RIGHT = 8,
   TREE_BASE = 9,
   TREE_TOP = 10,
-  WATER = 11
+  WATER = 11,
+  WATER_GROUND_TOP = 12,
+  WATER_GROUND_TOP_LEFT = 13,
+  WATER_GROUND_TOP_RIGHT = 14,
+  WATER_GROUND_LEFT = 15,
+  WATER_GROUND_RIGHT = 16,
+  WATER_GROUND_DIAGONAL_TOP_LEFT = 17,
+  WATER_GROUND_DIAGONAL_TOP_RIGHT = 18
 }
+
+const isGroundTile = (tile: GscTile) => (
+  tile >= GscTile.GRASS &&
+  tile <= GscTile.FLOWER_BOTTOM_RIGHT_1
+);
+
+const isTreeTile = (tile: GscTile) => (
+  tile === GscTile.TREE_BASE ||
+  tile === GscTile.TREE_TOP
+);
+
+const isWaterTile = (tile: GscTile) => (
+  tile >= GscTile.WATER &&
+  tile <= GscTile.WATER_GROUND_DIAGONAL_TOP_RIGHT
+);
 
 export default class GscWorldMap extends AbstractWorldMap<GscTile> {
   public constructor(seed: number, width: number, height: number) {
@@ -39,17 +61,24 @@ export default class GscWorldMap extends AbstractWorldMap<GscTile> {
         [GscTile.LEDGE_BOTTOM_RIGHT]: { x: 112, y: 80 },
         [GscTile.TREE_BASE]: { x: 224, y: 48 },
         [GscTile.TREE_TOP]: { x: 224, y: 32 },
-        [GscTile.WATER]: { x: 64, y: 16 }
+        [GscTile.WATER]: { x: 64, y: 16 },
+        [GscTile.WATER_GROUND_TOP]: { x: 304, y: 32 },
+        [GscTile.WATER_GROUND_TOP_LEFT]: { x: 320, y: 0 },
+        [GscTile.WATER_GROUND_TOP_RIGHT]: { x: 288, y: 0 },
+        [GscTile.WATER_GROUND_LEFT]: { x: 320, y: 16 },
+        [GscTile.WATER_GROUND_RIGHT]: { x: 288, y: 16 },
+        [GscTile.WATER_GROUND_DIAGONAL_TOP_LEFT]: { x: 320, y: 32 },
+        [GscTile.WATER_GROUND_DIAGONAL_TOP_RIGHT]: { x: 288, y: 32 }
       }
     });
   }
 
   protected buildWorldMap(): void {
-    const { width, height } = this.tileMap.getSize();
+    const mapSize = this.tileMap.getSize();
 
     // Generate flowers
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
+    for (let y = 0; y < mapSize.height; y++) {
+      for (let x = 0; x < mapSize.width; x++) {
         if (this.rng.random() > 0.8) {
           const flowerTile = this.rng.random() > 0.5 ? GscTile.FLOWER_BOTTOM_RIGHT_1 : GscTile.FLOWER_TOP_LEFT_1;
 
@@ -58,37 +87,23 @@ export default class GscWorldMap extends AbstractWorldMap<GscTile> {
       }
     }
 
-    const mapSize = this.tileMap.getSize();
-
     // Generate water
     for (let i = 0; i < 5000; i++) {
-      const pool: Area = {
-        x: this.rng.randomInRange(0, mapSize.width),
-        y: this.rng.randomInRange(0, mapSize.height),
-        width: this.rng.randomInRange(5, 15),
-        height: this.rng.randomInRange(5, 15)
-      };
+      const area = this.createRandomArea([8, 15]);
 
-      for (let y = pool.y; y < pool.y + pool.height; y++) {
-        for (let x = pool.x; x < pool.x + pool.width && x < mapSize.width; x++) {
-          this.tileMap.setTile({ x, y }, GscTile.WATER);
-        }
-      }
+      this.loopOver(area, ({ x, y }) => {
+        this.tileMap.setTile({ x, y }, GscTile.WATER);
+      });
     }
 
     // Generate tree clusters
     for (let i = 0; i < 5000; i++) {
-      const cluster: Area = {
-        x: this.rng.randomInRange(0, mapSize.width),
-        y: this.rng.randomInRange(0, mapSize.height),
-        width: this.rng.randomInRange(3, 20),
-        height: this.rng.randomInRange(3, 20)
-      };
+      const area = this.createRandomArea([3, 15]);
 
-      cluster.y = cluster.y % 2 === 0 ? cluster.y : cluster.y + 1;
+      area.y = area.y % 2 === 0 ? area.y : area.y + 1;
 
-      for (let y = cluster.y; y < cluster.y + cluster.height; y += 2) {
-        for (let x = cluster.x; x < cluster.x + cluster.width && x < mapSize.width; x++) {
+      for (let y = area.y; y < area.y + area.height; y += 2) {
+        for (let x = area.x; x < area.x + area.width && x < mapSize.width; x++) {
           const position: Point = { x, y };
 
           this.tileMap.setTile(position, GscTile.TREE_BASE);
@@ -99,25 +114,95 @@ export default class GscWorldMap extends AbstractWorldMap<GscTile> {
 
     // Generate poke grass
     for (let i = 0; i < 5000; i++) {
-      const patch: Area = {
-        x: this.rng.randomInRange(0, mapSize.width),
-        y: this.rng.randomInRange(0, mapSize.height),
-        width: this.rng.randomInRange(3, 10),
-        height: this.rng.randomInRange(3, 10)
-      };
+      const area = this.createRandomArea([3, 10]);
 
-      for (let y = patch.y; y < patch.y + patch.height; y++) {
-        for (let x = patch.x; x < patch.x + patch.width && x < mapSize.width; x++) {
-          const currentTile = this.tileMap.getTile({ x, y });
+      this.loopOver(area, ({ x, y }) => {
+        const currentTile = this.tileMap.getTile({ x, y });
 
-          if (
-            currentTile === GscTile.GRASS ||
-            currentTile === GscTile.FLOWER_BOTTOM_RIGHT_1 ||
-            currentTile === GscTile.FLOWER_TOP_LEFT_1
-          ) {
-            this.tileMap.setTile({ x, y }, GscTile.POKE_GRASS);
+        if (
+          currentTile === GscTile.GRASS ||
+          currentTile === GscTile.FLOWER_BOTTOM_RIGHT_1 ||
+          currentTile === GscTile.FLOWER_TOP_LEFT_1
+        ) {
+          this.tileMap.setTile({ x, y }, GscTile.POKE_GRASS);
+        }
+      });
+    }
+
+    // Generate shrubs
+    for (let y = 0; y < mapSize.height; y++) {
+      for (let x = 0; x < mapSize.width; x++) {
+        const tile = this.tileMap.getTile({ x, y });
+        const topTile = this.tileMap.getTile({ x, y: y - 1 });
+        const leftTile = this.tileMap.getTile({ x: x - 1, y });
+        const rightTile = this.tileMap.getTile({ x: x + 1, y });
+        const bottomTile = this.tileMap.getTile({ x, y: y + 1 });
+  
+        if (
+          isGroundTile(tile) &&
+          (
+            (isTreeTile(topTile) && y % 5 === 0) ||
+            (isTreeTile(leftTile) && x % 5 === 0) ||
+            (isTreeTile(rightTile) && x % 5 === 0) ||
+            (isTreeTile(bottomTile) && y % 5 === 0)
+          )
+        ) {
+          this.tileMap.setTile({ x, y }, GscTile.MINI_TREE);
+        }
+      }
+    }
+
+    // Add edges where water meets ground
+    for (let y = 0; y < mapSize.height; y++) {
+      for (let x = 0; x < mapSize.width; x++) {
+        const tile = this.tileMap.getTile({ x, y });
+
+        if (tile === GscTile.WATER) {
+          const topTile = this.tileMap.getTile({ x, y: y - 1 });
+          const leftTile = this.tileMap.getTile({ x: x - 1, y });
+          const rightTile = this.tileMap.getTile({ x: x + 1, y });
+          const topLeftTile = this.tileMap.getTile({ x: x - 1, y: y - 1 });
+          const topRightTile = this.tileMap.getTile({ x: x + 1, y: y - 1 });
+
+          if (!isWaterTile(topTile)) {
+            if (!isWaterTile(leftTile)) {
+              this.tileMap.setTile({ x, y }, GscTile.WATER_GROUND_TOP_LEFT);
+            } else if (!isWaterTile(rightTile)) {
+              this.tileMap.setTile({ x, y }, GscTile.WATER_GROUND_TOP_RIGHT);
+            } else {
+              this.tileMap.setTile({ x, y }, GscTile.WATER_GROUND_TOP);
+            }
+          } else if (!isWaterTile(leftTile)) {
+            this.tileMap.setTile({ x, y }, GscTile.WATER_GROUND_LEFT);
+          } else if (!isWaterTile(rightTile)) {
+            this.tileMap.setTile({ x, y }, GscTile.WATER_GROUND_RIGHT);
+          } else if (!isWaterTile(topLeftTile)) {
+            this.tileMap.setTile({ x, y }, GscTile.WATER_GROUND_DIAGONAL_TOP_LEFT);
+          } else if (!isWaterTile(topRightTile)) {
+            this.tileMap.setTile({ x, y }, GscTile.WATER_GROUND_DIAGONAL_TOP_RIGHT);
           }
         }
+      }
+    }
+  }
+
+  private createRandomArea(sizeRange: [number, number]): Area {
+    const mapSize = this.tileMap.getSize();
+
+    return {
+      x: this.rng.randomInRange(0, mapSize.width),
+      y: this.rng.randomInRange(0, mapSize.height),
+      width: this.rng.randomInRange(sizeRange[0], sizeRange[1]),
+      height: this.rng.randomInRange(sizeRange[0], sizeRange[1])
+    };
+  }
+
+  private loopOver(area: Area, handler: (point: Point) => void): void {
+    const mapSize = this.tileMap.getSize();
+
+    for (let y = area.y; y < area.y + area.height; y++) {
+      for (let x = area.x; x < area.x + area.width && x < mapSize.width; x++) {
+        handler({ x, y });
       }
     }
   }
